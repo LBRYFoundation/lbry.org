d626b12d96004c41071daa6b81cba2176a27c04e
Pigges <pigges@tutanota.com>
2022-12-30 13:13:35 +0100

    removed additinalPage for COTW

--- docs/.vuepress/additionalPages.js
+++ docs/.vuepress/additionalPages.js
@@ -6,7 +6,6 @@ module.exports = {
     const now = new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' });
 
     const projects = await rp('https://raw.githubusercontent.com/LBRYFoundation/Awesome-LBRY/master/README.md')
-    const cotw = await rp('https://lbry.pigg.es/cotw.md')
     return [
         {
             title: 'Awesome LBRY Projects',
@@ -14,12 +13,7 @@ module.exports = {
             editLink: true,
             editLinkPattern: ':LBRYFoundation/Awesome-LBRY',
             content: projects
-        },
-        {
-          title: 'Creator of the Week',
-          path: '/governance/creators-council/cotw/',
-          content: `# Creator Of The Week\nHere's a list of every channel that have been the creator of the week.\n\n*This page was last updated **${now}***\n\n${cotw}`
-      }
+        }
     ]
   }
 }
