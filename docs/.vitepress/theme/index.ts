d728b0133f6c315d543070ab23ad8819e9329904
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-11-10 15:39:57 +0100

    Fix redirecting

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,15 +7,33 @@ import TechButton from './components/TechButton.vue';
 import Layout from './Layout.vue';
 
 import './custom.css';
+import {EnhanceAppContext} from "vitepress/dist/client";
+
+import _redirects from "/_redirects?url&raw";
+const redirects = _redirects.split('\n').filter(String).map((item: string[]) => item.split(/\x20+/)).filter((item: string) => item[0].indexOf(':')===-1 && item[0].indexOf('*')===-1);
 
 /** @type {import('vitepress').Theme} */
 export default {
   Layout,
-  enhanceApp({ app }) {
+  enhanceApp({ app, router }: EnhanceAppContext) {
     app.component('Color',Color)
     app.component('IconifyIcon',Icon)
     app.component('Person',Person)
     app.component('TechButton',TechButton)
+
+    router.onAfterRouteChange = async (to: string) => {
+      // Static redirects
+      redirects.forEach((item: string[]) => {
+        if(to===item[0] || to.startsWith(item[0]+'?') || to.startsWith(item[0]+'#')){
+          router.go(item[1]);
+        }
+      });
+
+      // Dynamic redirects
+      if(to==='/faq' || to.startsWith('/faq/') || to.startsWith('/faq?') || to.startsWith('/faq#')){
+        await router.go('/archive'+to);
+      }
+    };
   },
   extends: DefaultTheme,
 };
